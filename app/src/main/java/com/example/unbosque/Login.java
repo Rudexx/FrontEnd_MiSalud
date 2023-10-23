0c1471769f965b3ec07097b46c4626b0b87bd158
Rudexx <dnogales@unbosque.edu.co>
2023-10-23 14:15:27 -0500

    interfaz login

---
+++ app/src/main/java/com/example/unbosque/Login.java
@@ -0,0 +1,44 @@
+package com.example.unbosque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.helloworld.R;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button login = findViewById(R.id.loginBtn);
+        login.setOnClickListener(this);
+        TextView register = findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.loginBtn) {
+            Toast.makeText(this, "Login :)", Toast.LENGTH_SHORT).show();
+        }else if(v.getId() == R.id.register){
+            Toast.makeText(this, "Register :)", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater  = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,  menu);
+        return true;
+    }
+}
